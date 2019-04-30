7cd080ecea0b041437c754d61321519dfc20bb90
DennisGoss99 <49404798+DennisGoss99@users.noreply.github.com>
2019-04-30 13:43:53 +0200

    Update cLogSystem.cpp

--- Programm/cLogSystem.cpp
+++ Programm/cLogSystem.cpp
@@ -13,39 +13,39 @@ void Log::Clear(void)
 {
 	clrscr();
 	time_t now = time(0);
-	tm *ltm = localtime(&now);
+	tm *ltm = localtime(&now);	//gives you the current time
 
-	fstream f;
-	f.open(OutputPath , ios::out);
-	f << ltm->tm_hour <<":";
+	fstream f;		
+	f.open(OutputPath , ios::out);	//opens file "OutputPath" and overwrites everything
+	f << ltm->tm_hour <<":";	
 	f << ltm->tm_min <<":";
-	f << ltm->tm_sec <<" ";
+	f << ltm->tm_sec <<" ";		//hours/minutes/seconds are written
 	f<<ltm->tm_mday<<"/"<<1+ltm->tm_mon<<"/";
 	f<<1900+ltm->tm_year<<" | ";
 
-	f << "cleared" <<endl;
-	f.close();
+	f << "cleared" <<endl;		//adds "cleared" to the file
+	f.close();			//Closes the file
 
 	getch();
 }
 
 void Log::Add(AnsiString LogContent)
 {
-	if (!enableLog) return;
+	if (!enableLog) return;		
 
 	clrscr();
 	time_t now = time(0);
-	tm *ltm = localtime(&now);
+	tm *ltm = localtime(&now);	//gives you the current time
 
 	fstream f;
-	f.open(OutputPath , ios::app);
+	f.open(OutputPath , ios::app);	//opens file OutputPath and adds it to the end
 	f << ltm->tm_hour <<":";
 	f << ltm->tm_min <<":";
-	f << ltm->tm_sec <<" ";
+	f << ltm->tm_sec <<" ";		//hours/minutes/seconds are written
 	f<<ltm->tm_mday<<"/"<<1+ltm->tm_mon<<"/";
 	f<<1900+ltm->tm_year<<" | ";
-	f << LogContent <<endl;
-	f.close();
+	f << LogContent <<endl;		//adds "LogContent" to the file
+	f.close();			//Closes the file
 
 	getch();
 }
@@ -56,24 +56,24 @@ void Log::Add(AnsiString LogContent,int Priority)
 
    	clrscr();
 	time_t now = time(0);
-	tm *ltm = localtime(&now);
+	tm *ltm = localtime(&now);	//gives you the current time
 
 	fstream f;
-	f.open(OutputPath , ios::app);
+	f.open(OutputPath , ios::app);	//opens file OutputPath and adds it to the end
 	f << ltm->tm_hour <<":";
 	f << ltm->tm_min <<":";
-	f << ltm->tm_sec <<" ";
+	f << ltm->tm_sec <<" ";		//hours/minutes/seconds are written
 	f<<ltm->tm_mday<<"/"<<1+ltm->tm_mon<<"/";
 	f<<1900+ltm->tm_year;
 	f << " [~" << Priority << "~] ";
-	f << LogContent <<endl;
-	f.close();
+	f << LogContent <<endl;		//adds "LogContent" to the file
+	f.close();			//Closes the file
 
 	getch();
 }
 
 
 void Log::OutputError(AnsiString Text ,AnsiString Caption , int Flag , bool DoLog){
-	if (DoLog) Add("Messagebox: " + Text + " | Caption: " + Caption);
+	if (DoLog) Add("Messagebox: " + Text + " | Caption: " + Caption);				//Wenn Log an ist wird der Msg-Kontent in den Log geschrieben
 	Application->MessageBox(cDBService.AnsiTowchar_t(Text),cDBService.AnsiTowchar_t(Caption),Flag);
 }
